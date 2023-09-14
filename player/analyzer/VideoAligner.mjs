469cf06c7b446f221d78d6687266099e6e450e30
Andrew S <andrews54757@gmail.com>
2023-09-13 21:38:51 -0500

    Edge case

--- player/analyzer/VideoAligner.mjs
+++ player/analyzer/VideoAligner.mjs
@@ -250,7 +250,10 @@ export class VideoAligner extends EventEmitter {
         if (timeEnd === timeStart) {
           filled = 1;
         }
-        if ( aligned.count > 8 && filled > 0.75 && Math.abs(this.currentSequence[indexEnd].time - timeEnd) <= 2) {
+        if (aligned.endB > memoryEntry.matchEnd) { // Exception for growing side
+          memoryEntry.matchEnd = aligned.endB;
+          this.hasMemoryChanges = true;
+        } else if (aligned.count > 8 && filled > 0.75 && Math.abs(this.currentSequence[indexEnd].time - timeEnd) <= 2) {
           if (aligned.startB !== memoryEntry.matchStart) {
             memoryEntry.matchStart = aligned.startB;
             this.hasMemoryChanges = true;
