aa3316340bbfb3a99c7e57a9ec3d60fb5980dd7b
Andrew S <andrews54757@gmail.com>
2023-09-19 15:20:00 -0500

    Fix perms

---
+++ chrome/perms.mjs
@@ -0,0 +1,38 @@
+async function updatePerms() {
+  const perms = await chrome.permissions.getAll();
+  const permsEl = document.querySelectorAll('.permstatus');
+  for (const el of permsEl) {
+    let hasPerms = false;
+    if (el.dataset.perm === 'all-urls') {
+      hasPerms = perms.origins[0] === '<all_urls>';
+    } else {
+      hasPerms = perms.permissions.includes(el.dataset.perm);
+    }
+
+    if (hasPerms) {
+      el.classList.add('has-perms');
+      el.classList.remove('no-perms');
+      el.textContent = 'Granted';
+    } else {
+      el.classList.remove('no-perms');
+      el.classList.add('no-perms');
+      el.textContent = 'Not Granted, click to grant';
+      el.addEventListener('click', () => {
+        if (el.dataset.perm === 'all-urls') {
+          chrome.permissions.request({
+            origins: ['<all_urls>'],
+          });
+        } else {
+          chrome.permissions.request({
+            permissions: [el.dataset.perm],
+          });
+        }
+      });
+    }
+  }
+}
+
+updatePerms();
+
+chrome.permissions.onAdded.addListener(updatePerms);
+chrome.permissions.onRemoved.addListener(updatePerms);
