c979760791aa06086b529eb3428cb358859ccb88
Andrew S <andrews54757@gmail.com>
2023-10-26 15:39:25 -0500

    Default based on computational resources

--- chrome/player/utils/EnvUtils.mjs
+++ chrome/player/utils/EnvUtils.mjs
@@ -10,6 +10,16 @@ export class EnvUtils {
   }
 
   static hasComputationalResources() {
-    return navigator.hardwareConcurrency >= 4;
+    // check cpu concurrency
+    if (navigator.hardwareConcurrency < 4) {
+      return false;
+    }
+
+    // check ram
+    if (navigator.deviceMemory !== undefined && navigator.deviceMemory < 8) {
+      return false;
+    }
+
+    return true;
   }
 }
