a0f6f4699d146d04b2c29abe84a5a5ee1e5e509b
Andrew S <andrews54757@gmail.com>
2023-11-22 09:05:21 -0600

    Fix intro/outro skip

--- chrome/player/ui/InterfaceController.mjs
+++ chrome/player/ui/InterfaceController.mjs
@@ -1323,6 +1323,9 @@ export class InterfaceController {
       });
     }
 
+    let currentSegment = null;
+    const time = this.client.currentTime;
+
     skipSegments.forEach((segment) => {
       const segmentElement = document.createElement('div');
       segmentElement.classList.add('skip_segment');
@@ -1330,18 +1333,19 @@ export class InterfaceController {
       segmentElement.style.left = segment.startTime / duration * 100 + '%';
       segmentElement.style.width = (segment.endTime - segment.startTime) / duration * 100 + '%';
       DOMElements.skipSegmentsContainer.appendChild(segmentElement);
+
+      if (!currentSegment && time >= segment.startTime && time < segment.endTime) {
+        currentSegment = segment;
+        segmentElement.classList.add('active');
+      }
     });
 
     this.skipSegments = skipSegments;
-
-    const time = this.client.currentTime;
-    const currentSegment = skipSegments.find((segment) => time >= segment.startTime && time < segment.endTime);
 
     if (currentSegment) {
       DOMElements.skipButton.style.display = '';
       DOMElements.skipButton.textContent = currentSegment.skipText;
       DOMElements.progressContainer.classList.add('skip_freeze');
-      currentSegment.classList.add('active');
     } else {
       DOMElements.progressContainer.classList.remove('skip_freeze');
       DOMElements.skipButton.style.display = 'none';
