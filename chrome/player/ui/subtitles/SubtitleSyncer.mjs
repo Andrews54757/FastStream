d72894d56c61d2cdcdf525448439063aa4926c39
Andrew S <andrews54757@gmail.com>
2023-11-22 09:45:45 -0600

    Fixes to subsyncer

--- chrome/player/ui/subtitles/SubtitleSyncer.mjs
+++ chrome/player/ui/subtitles/SubtitleSyncer.mjs
@@ -330,10 +330,10 @@ export class SubtitleSyncer extends EventEmitter {
       const el = WebUtils.create('div', '', 'timeline_track_cue');
       el.style.left = cue.startTime / this.video.duration * 100 + '%';
       el.style.width = (cue.endTime - cue.startTime) / this.video.duration * 100 + '%';
-      if (!cue.dom) {
-        cue.dom = WebVTT.convertCueToDOMTree(window, cue.text);
+      if (!cue.dom2) {
+        cue.dom2 = WebVTT.convertCueToDOMTree(window, cue.text);
       }
-      el.appendChild(cue.dom);
+      el.appendChild(cue.dom2);
       el.title = cue.text;
       this.ui.timelineTrack.appendChild(el);
 
