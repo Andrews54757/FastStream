9bb54950ffed4fc5f156e509db09e3b9de3a250e
Andrew S <andrews54757@gmail.com>
2023-11-27 03:04:25 -0600

    Add KeyT to override list

--- chrome/content.js
+++ chrome/content.js
@@ -444,6 +444,7 @@ if (is_url_yt(window.location.href)) {
     'KeyF', // fullscreen
     'KeyM', // mute
     'KeyJ', 'KeyL', // seek
+    'KeyT', // theater mode
     'Digit0', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', // seek
     'KeyC', // captions
   ];
