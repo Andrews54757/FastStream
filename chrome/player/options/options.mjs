d8f66e7468b9da11e34e6a10c9269f3e99ca003c
Andrew S <andrews54757@gmail.com>
2023-12-17 21:37:18 -0600

    Disable broken options on web version

--- chrome/player/options/options.mjs
+++ chrome/player/options/options.mjs
@@ -49,7 +49,9 @@ if (!EnvUtils.isExtension()) {
   playStreamURLs.disabled = true;
   playMP4URLs.disabled = true;
   autoSub.disabled = true;
+  autoplayYoutube.disabled = true;
   autoEnableURLSInput.disabled = true;
+  customSourcePatterns.disabled = true;
 }
 
 async function loadOptions(newOptions) {
