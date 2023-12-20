ff9a5ea3864bce6efe9236148185364e2712cf54
Andrew S <andrews54757@gmail.com>
2023-12-19 20:10:58 -0600

    Increase threshold to 0.25

--- chrome/player/ui/InterfaceController.mjs
+++ chrome/player/ui/InterfaceController.mjs
@@ -703,7 +703,7 @@ export class InterfaceController {
 
     document.addEventListener('visibilitychange', ()=>{
       const o = new IntersectionObserver(([entry]) => {
-        if (entry.intersectionRatio > 0.1) {
+        if (entry.intersectionRatio > 0.25) {
           this.handleVisibilityChange(true);
         } else {
           this.handleVisibilityChange(false);
