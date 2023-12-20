1635c4b7231fec95b5e987c17042e7bd95016663
Andrew S <andrews54757@gmail.com>
2023-12-19 20:45:57 -0600

    Miniplayer only works with extension

--- chrome/player/options/defaults/VisChangeActions.mjs
+++ chrome/player/options/defaults/VisChangeActions.mjs
@@ -1,6 +1,12 @@
+import {EnvUtils} from '../../utils/EnvUtils.mjs';
+
 export const VisChangeActions = {
   NOTHING: 'nothing',
   PLAY_PAUSE: 'playpause',
   MINI_PLAYER: 'miniplayer',
   // PIP: 'pip',
 };
+
+if (!EnvUtils.isExtension()) {
+  delete VisChangeActions.MINI_PLAYER;
+}
